db4ba4512b6a4f7c7a2e84fd007e0b26384b07a2
Junjie Yang <yjj31415926@gmail.com>
2014-04-02 23:44:46 +0800

    Signed-off-by: Junjie Yang <yjj31415926@gmail.com>

---
+++ urlclient.py
@@ -0,0 +1,10 @@
+__author__ = 'Yang'
+# High-Level Gopher Client with urllib
+
+import urllib,sys
+host=sys.argv[1]
+file=sys.argv[2]
+
+f=urllib.urlopen('gopher://%s%s' % (host,file))
+for line in f.readlines():
+    sys.stdout.write(line)
