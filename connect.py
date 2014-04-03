7ba1f0028da7473b8d4e8c1e6785a50b2ebf0d8c
Junjie Yang <yjj31415926@gmail.com>
2014-04-03 21:10:46 +0800

    Chapter 2

---
+++ connect.py
@@ -0,0 +1,12 @@
+__author__ = 'Yang'
+# Basic Conneciton
+
+import socket
+
+print("Creating socket...")
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print("done.")
+
+print("Connecting to remote host...")
+s.connect(("www.google.com",80))
+print("done.")
