fdd442f657c68bf6b32fd47c9c63395aafa43cb1
Junjie Yang <yjj31415926@gmail.com>
2014-04-07 00:05:58 +0800

    chapter 6

---
+++ dump_page.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+__author__ = 'yang'
+# Obtain Web Page
+import sys,urllib2
+
+req=urllib2.Request(sys.argv[1])
+fd=urllib2.urlopen(req)
+while 1:
+    data = fd.read(1024)
+    if not len(data):
+        break
+    sys.stdout.write(data)
